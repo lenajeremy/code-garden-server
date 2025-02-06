a32d19f00ab02d6c9af00ed202659fa5daf11465
Jeremiah Lena <jeremiahlena@Jeremiahs-MacBook-Pro-Evon.local>
2025-02-06 22:10:25 +0100

    updated languages

--- internal/services/docker/constants.go
+++ internal/services/docker/constants.go
@@ -4,28 +4,33 @@ type Language string
 
 var SupportedLanguages = []Language{
 	"python",
-	// "typescript", "go", "javascript", "ruby", "rust", "swift", "cpp",
+	"typescript",
+	"go",
+	"javascript",
+	"ruby",
+	"rust",
+	"swift",
+	"cpp",
 }
 
 var LanguageToImageMap = map[Language]string{
-	"python": "code-garden-python",
-	//"javascript": "code-garden-node",
-	//"typescript": "code-garden-node",
-	//"go":         "code-garden-go",
-	//"rust":       "code-garden-rust",
-	//"swift":      "code-garden-swift",
-	//"ruby":       "code-garden-ruby",
-	//"cpp": "code-garden-cpp",
+	"python":     "code-garden-python",
+	"javascript": "code-garden-node",
+	"typescript": "code-garden-node",
+	"go":         "code-garden-go",
+	"rust":       "code-garden-rust",
+	"swift":      "code-garden-swift",
+	"ruby":       "code-garden-ruby",
+	"cpp":        "code-garden-cpp",
 }
 
 var LanguageToDockerFileMap = map[Language]string{
-	"python": "Dockerfile_python",
-	//"javascript": "Dockerfile_node",
-	//"typescript": "Dockerfile_node",
-	//"go":         "Dockerfile_go",
-	//"rust":       "Dockerfile_rust",
-	//"ruby":       "Dockerfile_ruby",
-	//"swift":      "Dockerfile_swift",
-	//"cpp": "Dockerfile_cpp",
-	//"
+	"python":     "Dockerfile_python",
+	"javascript": "Dockerfile_node",
+	"typescript": "Dockerfile_node",
+	"go":         "Dockerfile_go",
+	"rust":       "Dockerfile_rust",
+	"ruby":       "Dockerfile_ruby",
+	"swift":      "Dockerfile_swift",
+	"cpp":        "Dockerfile_cpp",
 }
